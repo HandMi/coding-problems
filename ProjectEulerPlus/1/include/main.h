4caedf7c649145c47a331f0a1623a6524aa3e4c4
HandMi <Michael.Handrek@gmail.com>
2020-10-13 02:25:35 +0200

    Add PEP1

---
+++ ProjectEulerPlus/1/include/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <algorithm>
+#include <vector>
+
+long SumOfMultiplesOf3And5(int n);
+
+#endif
---
+++ ProjectEulerPlus/PEP1/test/1_test.cpp
@@ -0,0 +1,29 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "main.h"
+
+class PEP1_SumOfMultiplesOf3And5Test
+    : public testing::TestWithParam<std::pair<int, long long>> {};
+
+TEST_P(PEP1_SumOfMultiplesOf3And5Test, SumOfMultiplesOf3And5Test) {
+  auto n = GetParam().first;
+  auto expected = GetParam().second;
+  auto result = SumOfMultiplesOf3And5(n);
+  EXPECT_EQ(result, expected);
+}
+
+// clang-format off
+INSTANTIATE_TEST_CASE_P(
+    PEP1_SumOfMultiplesOf3And5Tests, PEP1_SumOfMultiplesOf3And5Test,
+    ::testing::Values(std::make_pair(2,0),
+                      std::make_pair(4,3),
+                      std::make_pair(6,8),
+                      std::make_pair(10,23),
+                      std::make_pair(100,2318),
+                      std::make_pair(1000000001,233333334166666668)));
+// clang-format on
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
