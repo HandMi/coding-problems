231f39bb2864c180cda3cdcc4d5572dc61a2132c
HandMi <Michael.Handrek@gmail.com>
2020-10-16 03:36:17 +0200

    Add CF1427B

---
+++ Codeforces/CF1427B/src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <unistd.h>
+
+int CountFillableGaps(std::map<int, int> gaps, int k) {
+  int filled_gaps = 0;
+  for (const auto& gap : gaps) {
+    int possibly_filled_gaps = std::min(k / gap.first, gap.second);
+    filled_gaps += possibly_filled_gaps;
+    k -= possibly_filled_gaps * gap.first;
+  }
+  return filled_gaps;
+}
+
+int MaxScore(const std::string& games, int k) {
+  auto next_win = games.find('W');
+  if (next_win < games.size()) {
+    int streaks = 0;
+    int wins = 0;
+    std::map<int, int> gaps;
+    while(next_win < games.size()) {
+      ++streaks;
+      auto next_loss = std::min(games.find('L', next_win),games.size());
+      wins+=next_loss-next_win;
+      next_win = std::min(games.find('W', next_loss),games.size());
+      if(next_win<games.size()){
+          ++gaps[next_win-next_loss];
+      }
+    }
+    if ((wins + k) >= games.length()) {
+      return 2 * games.length() - 1;
+    }
+    return 2 * (k + wins) - streaks + CountFillableGaps(gaps, k);
+  } else {
+    return std::max(0, 2 * k - 1);
+  }
+}
+
+void Solve(std::istream& in, std::ostream& out) {
+  int t;
+  in >> t;
+  for (auto i = 0U; i < t; ++i) {
+    int n, k;
+    in >> n;
+    in >> k;
+    std::string games;
+    in >> games;
+    out << MaxScore(games, k) << std::endl;
+  }
+}
+
+int main() {
+  Solve(std::cin, std::cout);
+  return 0;
+}
---
+++ Codeforces/CF1427B/test/CF1427B_test.cpp
@@ -0,0 +1,39 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "main.h"
+#include <fstream>
+
+class CF1427B_ChessCheaterTest
+    : public testing::TestWithParam<std::pair<std::string, std::string>> {};
+
+TEST_P(CF1427B_ChessCheaterTest, ChessCheaterTest) {
+  auto input_file = GetParam().first;
+  auto expected_file = GetParam().second;
+  std::ifstream input_ifs(input_file);
+  std::ifstream expected_ifs(expected_file);
+  std::stringstream output;
+
+  Solve(input_ifs, output);
+
+  for (std::string expected_line; std::getline(expected_ifs, expected_line);) {
+    std::string output_line;
+    std::getline(output, output_line);
+    EXPECT_EQ(output_line, expected_line);
+  }
+}
+
+// clang-format off
+INSTANTIATE_TEST_CASE_P(
+    CF1427B_ChessCheaterTests, CF1427B_ChessCheaterTest,
+    ::testing::Values(std::make_pair("Codeforces/CF1427B/data/0_in.txt","Codeforces/CF1427B/data/0_out.txt"),
+                      std::make_pair("Codeforces/CF1427B/data/1_in.txt","Codeforces/CF1427B/data/1_out.txt"),
+                      std::make_pair("Codeforces/CF1427B/data/2_in.txt","Codeforces/CF1427B/data/2_out.txt"),
+                      std::make_pair("Codeforces/CF1427B/data/3_in.txt","Codeforces/CF1427B/data/3_out.txt"),
+                      std::make_pair("Codeforces/CF1427B/data/4_in.txt","Codeforces/CF1427B/data/4_out.txt"),
+                      std::make_pair("Codeforces/CF1427B/data/5_in.txt","Codeforces/CF1427B/data/5_out.txt")));
+// clang-format on
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
